50dad9889abaa235d809488bccbc4c789bb63a7f
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-07-29 11:46:11 -0300

    função de requisição

--- requisicoes/src/App.jsx
+++ requisicoes/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {data, setData} = useState('')
+
+  useEffect(() => {
+   async function getData(){
+      try {
+        const dados = await fetch("http://localhost:3000/games")
+        
+        if(!dados.ok) {
+          throw new Error('Não foi possivel obter dados')
+        }
+        
+        console.log(await dados.json())
+        // toast.success('dados obtidos com sucesso')
+        
+      } catch (error) {
+        toast.error(error.message)
+        // console.log(error)
+      }
+    }
+
+    getData()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao Meu Projeto React</h1>
+        <p>Esta é uma página inicial simples para começar a trabalhar com requisições em React.</p>
+        <button>Fazer Requisição</button>
+        <ToastContainer />
+      </header>
+    </div>
   )
 }
 
