7d374267659a1fc5b4702ed26b35a8eb6a47b590
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-08-14 08:50:02 -0300

    Nova seção, react router

---
+++ reactRouter/src/components/nav/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/services" className="nav-link">Services</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
