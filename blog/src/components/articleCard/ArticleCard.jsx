a1a447681397b728cac6f82b12e7eecfb0e8a1b3
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-10-14 14:33:20 -0300

    editei blog

---
+++ blog/src/components/articleCard/ArticleCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './ArticleCard.css'
+
+function ArticleCard() {
+    return (
+        <article>
+            <figure>
+                <img src="https://dbriefing.com.br/blog/wp-content/uploads/2017/10/blog-1.jpg" alt="" />
+            </figure>
+            <h1 className='title-post'>Post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, debitis eligendi. Adipisci consequatur dolores veniam at. In dolor odit, quas maxime sunt culpa. Porro provident dicta eligendi placeat veritatis laboriosam, et perspiciatis eius perferendis eos quia vero expedita natus, harum voluptate hic officia cum cumque minima eveniet fugit deleniti? Quae placeat itaque et in! Aperiam saepe necessitatibus cum, qui vero, quas facere quis debitis architecto voluptatem sapiente aspernatur repudiandae, aut perspiciatis beatae. Laudantium ullam ducimus, illo nam mollitia corrupti non amet quos quod optio qui molestiae, doloremque hic nostrum dolor doloribus laboriosam maiores deserunt nulla incidunt esse voluptate culpa. Deserunt nisi laborum nostrum itaque quaerat ipsum omnis officiis doloremque libero repudiandae perspiciatis obcaecati ullam tempore illum illo pariatur optio, iste rem, dolorum veniam laudantium. Rem necessitatibus facilis animi ea totam omnis, consequuntur itaque soluta. Necessitatibus natus velit odit excepturi id autem, vero, exercitationem nobis incidunt, voluptas odio eum cupiditate voluptatem.</p>
+        </article>
+    )
+}
+
+export default ArticleCard
--- blog/src/pages/Home.jsx
+++ blog/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import './Home.css'
 import TopicsMenu from '../components/TopicsMenu/TopicsMenu'
 import Titles from '../components/titleSections/Titles'
 import Menu from '../components/menu/Menu'
+import ArticleCard from '../components/articleCard/ArticleCard'
 
 function Home() {
     return (
@@ -12,13 +13,7 @@ function Home() {
             <Titles />
             <section className='container container-home'>
                 <main className='article-container'>
-                    <article>
-                        <figure>
-                            <img src="https://dbriefing.com.br/blog/wp-content/uploads/2017/10/blog-1.jpg" alt="" />
-                        </figure>
-                        <h1 className='title-post'>Post</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, debitis eligendi. Adipisci consequatur dolores veniam at. In dolor odit, quas maxime sunt culpa. Porro provident dicta eligendi placeat veritatis laboriosam, et perspiciatis eius perferendis eos quia vero expedita natus, harum voluptate hic officia cum cumque minima eveniet fugit deleniti? Quae placeat itaque et in! Aperiam saepe necessitatibus cum, qui vero, quas facere quis debitis architecto voluptatem sapiente aspernatur repudiandae, aut perspiciatis beatae. Laudantium ullam ducimus, illo nam mollitia corrupti non amet quos quod optio qui molestiae, doloremque hic nostrum dolor doloribus laboriosam maiores deserunt nulla incidunt esse voluptate culpa. Deserunt nisi laborum nostrum itaque quaerat ipsum omnis officiis doloremque libero repudiandae perspiciatis obcaecati ullam tempore illum illo pariatur optio, iste rem, dolorum veniam laudantium. Rem necessitatibus facilis animi ea totam omnis, consequuntur itaque soluta. Necessitatibus natus velit odit excepturi id autem, vero, exercitationem nobis incidunt, voluptas odio eum cupiditate voluptatem.</p>
-                    </article>
+                    <ArticleCard />
                 </main>
                 <aside>
                     <div className="recent-post-card">
